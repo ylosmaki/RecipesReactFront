11677819be2ca1b005972529c0aced614ad73cb6
MaijaYlosmaki <maija.ylosmaki@gmail.com>
2020-02-27 15:25:13 +0200

    meals on sidebar has onclick, incredients show up as result on result.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,35 @@ import Search from "./Search"
 import Results from "./Results"
 import Footer from "./Footer"
 
-function App() {
-  return (
+class App extends React.Component{
+    state = {meals : [], selectedMeal: null}
+    componentDidMount() {
+        fetch('http://localhost:8080/api/meals')
+            .then(response => response.json())
+            .then((response) => this.setState(
+                {meals: response}))
+    }
+    selectMeal = (id) => {
+        console.log("Selecting meal", id)
+        for(let m of this.state.meals) {
+            if(m.id === id) {
+                console.log("Found a match", m)
+                this.setState({selectedMeal: m});
+                return
+            }
+        }
+    }
+    render() {
+    return (
     <div className="App">
         <Header /> {/*Header and stuff*/}
         <Search /> {/*what do you want to search from database*/}
-        <Sidebar /> {/*meals*/}
-        <Results /> {/*recipes and search result*/}
+        <Sidebar meals={this.state.meals} selectMeal={this.selectMeal}/> {/*meals*/}
+        <Results meal={this.state.selectedMeal}/> {/*recipes and search result*/}
         <Footer /> {/*footer if we need one*/}
 
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/ShowMeal.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from "react"
+
+class ShowMeal extends Component {
+    meWasClicked = () => {
+        console.log("Clicked me, id", this.props.list.id)
+        this.props.selectMeal(this.props.list.id);
+    }
+    render () {
+        return (
+                <h3 className="clickable" onClick={this.meWasClicked}>{this.props.list.name}</h3>
+        )
+    }
+}
+
+export default ShowMeal
